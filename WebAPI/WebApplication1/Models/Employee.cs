98e6cc151989e0d88b964642f8b47dabd8d7d9e1
Avinash Jumrani <avinash.jumrani@tatvasoft.com>
2024-02-17 00:37:50 +0530

    first commit

---
+++ WebAPI/WebApplication1/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public int EmployeeCode { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string DOB { get; set; }
+        public int Age { get; set; }
+        public string skills { get; set; }
+        public string address { get; set; }
+        public string Department { get; set; }
+        public string Status { get; set; }
+
+
+
+     
+
+    }
+}
